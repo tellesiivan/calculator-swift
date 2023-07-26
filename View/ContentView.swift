6aa6c94d65f131a764c1bbf15215ca74d0afc93c
ivan <tellezivan7@gmail.com>
2023-07-25 23:01:10 -0700

    feat: add calculation logic

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+       ZStack{
+          LinearGradient(gradient: Gradient(colors: [.hOperator, .vOperator.opacity(0.4)]), startPoint: .top, endPoint: .bottom)
+             .ignoresSafeArea()
+             KeyView()
+       }
     }
 }
 
--- View/KeyView.swift
+++ View/KeyView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct KeyView: View {
-   @State var placeholderValue: String = Keys.zero.rawValue
+   @State var value: String = Keys.zero.rawValue
    @State var calculatedValue: Int = 0
    @State var currentOperation: Operations = .none
    
@@ -28,49 +28,99 @@ struct KeyView: View {
           
           HStack {
              RoundedRectangle(cornerRadius: 15)
-                .foregroundColor(animateRecColor ? Color(.numKeys) : Color.pink.opacity(0.2))
-                .scaleEffect(animateRecColor ? 1.5 : 1.0)
-                .frame(width: 350, height: 280)
-                .animation(.spring.speed(0.10).repeatForever(), value: animateRecColor)
+                .foregroundColor(animateRecColor ? Color(.white).opacity(0.4) : Color.vOperator)
+                .scaleEffect(animateRecColor ? 10 : 1.0)
+                .frame(width: 350, height: 260)
+                .animation(.easeOut(duration: 2.2), value: animateRecColor)
                 .onAppear(perform: {
                    self.animateRecColor.toggle()
                 })
-                .overlay(Text(placeholderValue)
+                .overlay(Text(value)
                   .bold()
                   .font(.system(size: 100))
              )
           }
-          ForEach(buttons, id: \.self) { keysGroup in
-             HStack(spacing: 10){
-                ForEach(keysGroup, id:\.self) { key in
-                   Button(action: {
-                      self.didTap(key)
-                   }, label: {
-                      Text(key.rawValue)
-                         .font(.system(size: 26))
-                         .fontWeight(.semibold)
-                         .frame(width: self.getWidth(elem: key), height: self.getHeight(elem: key))
-                         .background(key.buttonColor)
-                         .foregroundColor(key.foregroundColor)
-                         .cornerRadius(self.getWidth(elem: key) / 2)
-                         .shadow(color:.gray.opacity(0.24), radius:6 )
-                   })
+          VStack {
+             ForEach(buttons, id: \.self) { keysGroup in
+                HStack(spacing: 10){
+                   ForEach(keysGroup, id:\.self) { key in
+                      Button(action: {
+                         self.didTap(key)
+                      }, label: {
+                         Text(key.rawValue)
+                            .font(.system(size: 26))
+                            .fontWeight(.semibold)
+                            .frame(width: self.getWidth(elem: key), height: self.getHeight(elem: key))
+                            .background(key.buttonColor)
+                            .foregroundColor(key.foregroundColor)
+                            .cornerRadius(self.getWidth(elem: key) / 2)
+                            .shadow(color:.gray.opacity(0.24), radius:3 )
+                      })
+                   }
                 }
-             }.padding(.bottom, 8)
-          }
+             }
+          }.padding(10)
        }
-       .padding(10)
+       
     }
    
    
    func didTap(_ button: Keys) {
-      print("Button")
+      switch button {
+         case .add, .subtract, .multiply, .equal, .divide: do {
+            if button == .add {
+               self.currentOperation = .add
+               self.calculatedValue = Int(self.value) ?? 0
+            } else if button == .subtract {
+               self.currentOperation = .subtract
+               self.calculatedValue = Int(self.value) ?? 0
+            } else if button == .multiply {
+               self.currentOperation = .multiply
+               self.calculatedValue = Int(self.value) ?? 0
+            } else if button == .divide {
+               self.currentOperation = .divide
+               self.calculatedValue = Int(self.value) ?? 0
+            } else if button == .equal {
+               let calculatedValue = self.calculatedValue
+               let currentValue = Int(self.value) ?? 0
+               
+               switch self.currentOperation {
+                  case .add:
+                     self.value = "\(calculatedValue + currentValue)"
+                  case .subtract:
+                     self.value = "\(calculatedValue - currentValue)"
+                  case .multiply:
+                     self.value = "\(calculatedValue * currentValue)"
+                  case .divide:
+                     self.value = "\(calculatedValue / currentValue)"
+                  case .none:
+                     break
+               }
+            }
+         }
+            if button != .equal {
+               self.value = "0"
+            }
+         case .clear:
+            self.value = "0"
+         case .decimal, .negative, .percent:
+            break
+         default:
+            let num = button.rawValue
+            if self.value == "0" {
+               value = num
+            } else {
+               self.value = "\(self.value)\(num)"
+            }
+      }
    }
    
    func getHeight(elem: Keys) -> CGFloat {
       return (UIScreen.main.bounds.width - (5 * 10)) / 4
    }
    
+   
+
    func getWidth(elem: Keys) -> CGFloat {
 //      since the num .zero button we want half of the width, we divide by 2(half of the screen)
       if elem == .zero {
